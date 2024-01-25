28d61bfeb1394d2e1ae869dee61bc0305c7857d9
David Castillo Mora <156556862+Davidcm2803@users.noreply.github.com>
2024-01-25 00:10:16 -0600

    Add files via upload

---
+++ Poyecto_main/README.md
@@ -0,0 +1,2 @@
+# Bus-tour
+Bus tour from our capital San José Costa Rica, using java
---
+++ Poyecto_main/src/Main_proyecto/Main.java
@@ -0,0 +1,16 @@
+
+package Main_proyecto;
+
+import bus_inter.recorrido;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        recorrido carrera = new recorrido();
+    }
+}
